3310680ba584a458c5d6f4617de9e39834a3c280
Confidence Uzu <113725251+uzuthedev@users.noreply.github.com>
2025-01-23 22:48:12 -0700

    ..

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Page() {
     <main>
       <h1 className="text-4xl font-bold mb-5">CPRG 306: Web Development 2 - Assignments</h1>
       <p>
-        <Link href="/week-2"> Week 2 assignment</Link>
+        <Link href="/week-2"> Week 2 assignment</Link> 
       </p>
     </main>
   );
