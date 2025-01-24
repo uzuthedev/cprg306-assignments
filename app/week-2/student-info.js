5aece0e4800876183a4c2b9ae336ea06ad0dbbcd
Confidence Uzu <113725251+uzuthedev@users.noreply.github.com>
2025-01-23 20:55:33 -0700

    update

---
+++ app/week-2/student-info.js
@@ -0,0 +1,17 @@
+// student-info.js
+import Link from 'next/link';
+
+function StudentInfo() {
+  return (
+    <div>
+      <h1>Chukbikem Confidence</h1>
+      <p>
+        <Link href="https://github.com/uzuthedev/cprg306-assignments">
+          My GitHub Repository
+        </Link>
+      </p>
+    </div>
+  );
+}
+
+export default StudentInfo;
