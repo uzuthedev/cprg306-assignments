551aef727aa90e15d283ab3d39b2642bb90a7ab8
Confidence Uzu <113725251+uzuthedev@users.noreply.github.com>
2025-01-23 21:57:23 -0700

    .

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Page() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <h1 className="text-4xl font-bold mb-5">CPRG 306: Web Development 2 - Assignments</h1>
       <p>
         <Link href="/week-2"> Week 2 assignment</Link>
       </p>
