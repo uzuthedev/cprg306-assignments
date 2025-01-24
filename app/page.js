633922af89a8f48005a64a0fd7c2dc3fce4e438d
Confidence Uzu <113725251+uzuthedev@users.noreply.github.com>
2025-01-23 22:07:21 -0700

    ...

--- app/page.js
+++ app/page.js
@@ -5,14 +5,12 @@ export default function Page() {
     <main>
       <h1 className="text-4xl font-bold mb-5">CPRG 306: Web Development 2 - Assignments</h1>
       <div className="text-lg">
-          <ul>
-            {weekDemos.map((week) => (
-              <li key={week} className="hover:text-green-400 hover:underline">
-               <Link href="/week-2"> Week 2 assignment</Link> 
-              </li>
-            ))}
-          </ul>
-        </div>
+        <ul>
+          <li className="hover:text-green-400 hover:underline">
+            <Link href="/week-2">Week 2 Assignment</Link>
+          </li>
+        </ul>
+      </div>
     </main>
   );
 }
