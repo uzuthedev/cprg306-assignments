ed7a65b2aa43103ff30e36ce548edf370bd2fb92
Confidence Uzu <113725251+uzuthedev@users.noreply.github.com>
2025-01-27 22:53:04 -0700

    new changes
    week 3 and 4

--- app/page.js
+++ app/page.js
@@ -1,12 +1,39 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-export default function Page() {
+export default function Home() {
+
+  let linkStyle = "underline text-cyan-700 hover:text-cyan-950"
   return (
     <main>
-      <h1 className="text-4xl font-bold mb-5">CPRG 306: Web Development 2 - Assignments</h1>
-      <p>
-        <Link href="/week-2"> Week 2 assignment</Link> 
-      </p>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <ul>
+        <li>
+          <Link className={linkStyle} href="week-2">Week 2 Assignment</Link>
+        </li>
+        <li>
+          <Link className={linkStyle} href="week-3">Week 3 Assignment</Link>
+        </li>
+        <li>
+          <Link className={linkStyle} href="week-4">Week 4 Assignment</Link>
+        </li>
+        <li>
+        <Link className={linkStyle} href="week-5">Week 5 Assignment</Link>
+
+        </li>
+        <li>
+          <Link className={linkStyle} href="week-6">Week 6 Assignment</Link>
+        </li>
+        <li>
+          <Link className={linkStyle} href="week-7">Week 7 Assignment</Link>
+        </li>
+        <li>
+          <Link className={linkStyle} href="week-8">Week 8 Assignment</Link>
+        </li>
+        <li>
+          <Link className={linkStyle} href="week-9">Week 9 Assignment</Link>
+        </li>
+        
+      </ul>
     </main>
   );
 }
---
+++ app/week-3/page.js
@@ -0,0 +1,9 @@
+import ItemsList from "./item-list";
+export default function Page(){
+    return(
+        <main className="bg-green-900 w-1/2 m-auto">
+            <ItemsList/>
+        </main>
+    );
+
+}
