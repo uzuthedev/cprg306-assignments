432079a8e72e44b8ff6ed29c1d7a790a95d8acbb
Confidence Uzu <113725251+uzuthedev@users.noreply.github.com>
2025-01-27 23:08:53 -0700

    new changes
    mmm

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,9 +1,10 @@
-import ItemsList from "./item-list";
-export default function Page(){
-    return(
-        <main className="bg-green-900 w-1/2 m-auto">
-            <ItemsList/>
-        </main>
-    );
+import ItemList from "./item-list";
 
+export default function Page() {
+  return (
+    <main className="p-6 max-w-4xl mx-auto">
+      <h1 className="text-3xl font-bold mb-6 text-center text-teal-700">Shopping List</h1>
+      <ItemList />
+    </main>
+  );
 }
